527642576dc415bc7e07765c0bed2323d7438bc2
zahaev <113417779+zahaev@users.noreply.github.com>
2024-11-20 14:27:55 +0300

    Update controller.py

--- 3.1-3.2/controller.py
+++ 3.1-3.2/controller.py
@@ -11,24 +11,9 @@ class MainController:
     def fetch_data(self):
         data = self.model.get_data_from_db()  # Получаем данные из базы данных
         self.view.update_data(data)  # Обновляем представление с новыми данными
+    def update(self, data)
+        self.view.update_data(data) # Обновляем данные в представлении, когда модель уведомляет об изменениях
 
-    def open_add_patient_window(self):
-        patient_controller = PatientController(self.model)#,self.controller)  # Создаём контроллер для нового окна
-        PatientView(self.view.window, patient_controller)  # Создаём окно с контроллером
-
-    def open_patients_list_window(self):
-        patients_list_controller = PatientsListController(self.model)#,self.controller)  # Создаём контроллер для списка пациентов
-        PatientsListView(self.view.window, patients_list_controller)   # Создаём окно для просмотра пациентов
-
-        # -------------------------------сделать ссылку на другие конторллеры---------------------------------------------
-    #def add_patient(self, patient_data):
-       #return self.model.add_patient(patient_data)
-    #def get_patients(self):
-        #return self.model.get_data_from_db()  # Получаем список пациентов из модели
-
-    #def update(self, data):
-        # Обновляем данные в представлении, когда модель уведомляет об изменениях
-        #self.view.update_data(data)
 class PatientController:
     def __init__(self, model,controller):
         self.model = model
@@ -37,14 +22,13 @@ class PatientController:
         return self.model.add_patient(patient_data)
 
 class PatientsListController:
-    def __init__(self, model,controller):
+    def __init__(self, model,controller,view):
         self.model = model
         self.controller = controller
+        self.view = view
 
-        def get_patients(self):
-            return self.model.get_data_from_db()  # Получаем список пациентов из модели
+    def get_patients(self):
+        return self.model.get_data_from_db()  # Получаем список пациентов из модели
 
-        def update(self, data):
-            self.view.update_data(data) # Обновляем данные в представлении, когда модель уведомляет об изменениях
 
 
